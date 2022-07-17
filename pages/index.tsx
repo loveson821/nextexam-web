902008f35755e55210c499c5439bd4b0969b433d
loveson821 <loveson821@gmail.com>
2022-07-17 11:37:44 +0800

    pdf viewer

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,38 +5,41 @@ import { useEffect, useRef, useState } from 'react'
 import Footer from './components/footer'
 import Header from './components/header'
 
-import { Viewer } from '@react-pdf-viewer/core';
-import '@phuocng/react-pdf-viewer/cjs/react-pdf-viewer.css';
-import { defaultLayoutPlugin } from '@react-pdf-viewer/default-layout';
-// Import styles
+import { Viewer, Worker } from '@react-pdf-viewer/core';
+// import { defaultLayoutPlugin } from '@react-pdf-viewer/default-layout';
+
 import '@react-pdf-viewer/core/lib/styles/index.css';
 import '@react-pdf-viewer/default-layout/lib/styles/index.css';
 
+// const defaultLayoutPluginInstance = defaultLayoutPlugin();
+
+import dynamic from "next/dynamic";
+
+// const PDFViewer = dynamic(() => import("@react-pdf-viewer/core"), {
+//   ssr: false
+// });
+
 const Home: NextPage = () => {
-  useEffect(() =>{
-    
+  useEffect(() => {
 
-  },[])
 
-  const defaultLayoutPluginInstance = defaultLayoutPlugin();
+  }, [])
+
+  // http://examhero.com/uploads/book_section/pdf/99/四校序及專家團隊名單__20200330.pdf
+  // const defaultLayoutPluginInstance = defaultLayoutPlugin();
   return (
     <div className=' min-h-screen h-full relative'>
-      <Header/>
-      <Viewer 
-        fileUrl="http://examhero.com/uploads/book_section/pdf/99/四校序及專家團隊名單__20200330.pdf" 
-        plugins={[
-          // Register plugins
-          defaultLayoutPluginInstance
-        ]}
-      />
-      {/* <Worker workerUrl="https://unpkg.com/pdfjs-dist@2.4.456/build/pdf.worker.min.js">
-
-      </Worker> */}
-      {/* <Worker workerUrl="https://unpkg.com/pdfjs-dist@2.4.456/build/pdf.worker.min.js">
+      <Header />
+      <Worker workerUrl="https://unpkg.com/pdfjs-dist@2.14.305/build/pdf.worker.js">
         <div style={{ height: '750px' }}>
-            <Viewer fileUrl="http://examhero.com/uploads/book_section/pdf/99/四校序及專家團隊名單__20200330.pdf" />
+          <Viewer
+            fileUrl="https://examhero.com/uploads/book_section/pdf/99/四校序及專家團隊名單__20200330.pdf"
+            plugins={[
+              // defaultLayoutPluginInstance,
+            ]}
+          />
         </div>
-    </Worker> */}
+      </Worker>
     </div>
   )
 }
