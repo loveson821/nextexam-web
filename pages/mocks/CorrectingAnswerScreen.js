11334c85fdb961d548adf4bc384133ed2bd833e1
loveson821 <loveson821@gmail.com>
2022-07-18 20:14:50 +0800

    改卷有背景

--- pages/mocks/CorrectingAnswerScreen.js
+++ pages/mocks/CorrectingAnswerScreen.js
@@ -1,9 +1,11 @@
-import React, { Component,useEffect } from "react";
+import React, { Component, useEffect } from "react";
 import { render } from "react-dom";
 
 import CanvasDraw from "react-canvas-draw";
 import Router, { useRouter } from "next/router";
 // import classNames from "./index.css";
+
+import html2canvas from "html2canvas";
 
 export default class CorrectingAnswerScreen extends Component {
   // router = useRouter()
@@ -13,20 +15,21 @@ export default class CorrectingAnswerScreen extends Component {
     height: 400,
     brushRadius: 1,
     lazyRadius: 2,
-    backgroundImg: "https://upload.wikimedia.org/wikipedia/commons/a/a1/Nepalese_Mhapuja_Mandala.jpg",
+    backgroundImg:
+      "https://upload.wikimedia.org/wikipedia/commons/a/a1/Nepalese_Mhapuja_Mandala.jpg",
     imgs: [
       "https://upload.wikimedia.org/wikipedia/commons/a/a1/Nepalese_Mhapuja_Mandala.jpg",
-      "https://i.imgur.com/a0CGGVC.jpg"
-    ]
+      "https://i.imgur.com/a0CGGVC.jpg",
+    ],
   };
-  
+
   render() {
     // const router = useRouter()
     // console.log(url);
     return (
       <div id="demo">
         <p>Try it out! Draw something, hit "Save" and then "Load".--</p>
-        <div className='flex justify-between '>
+        <div className="flex justify-between ">
           <button
             onClick={() => {
               localStorage.setItem(
@@ -54,23 +57,31 @@ export default class CorrectingAnswerScreen extends Component {
           </button>
           <button
             onClick={() => {
-              console.log(this.saveableCanvas.getDataURL());
+              // this.saveableCanvas.getDataURL("png", true, "white")
+              // console.log(this.saveableCanvas.getDataURL("png", true, "white"));
               // alert("DataURL written to console")
+              html2canvas(document.querySelector("#drawing-area")).then(canvas => {
+                console.log(canvas.toDataURL())
+              });
+
             }}
           >
             GetDataURL
           </button>
         </div>
-        <CanvasDraw
-          ref={canvasDraw => (this.saveableCanvas = canvasDraw)}
-          brushColor={this.state.color}
-          brushRadius={this.state.brushRadius}
-          lazyRadius={this.state.lazyRadius}
-          canvasWidth={this.state.width}
-          canvasHeight={this.state.height}
-          imgSrc={this.state.backgroundImg}
-        />
 
+        <div id="drawing-area" style={{ display: "inline-flex" }}>
+          <CanvasDraw
+            style={{ display: "inline-flex" }}
+            ref={(canvasDraw) => (this.saveableCanvas = canvasDraw)}
+            brushColor={this.state.color}
+            brushRadius={this.state.brushRadius}
+            lazyRadius={this.state.lazyRadius}
+            canvasWidth={this.state.width}
+            canvasHeight={this.state.height}
+            imgSrc={this.state.backgroundImg}
+          />
+        </div>
 
         <p>
           That's it for now! Take a look at the{" "}
